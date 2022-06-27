990d0a13f9f88905ff82dbedcac4eb9d882b67d5
Danya_Vid <danilviduykov@gmail.com>
2022-06-27 10:05:51 +0500

    lesson 5 - button back

--- src/pages/SinglePage.tsx
+++ src/pages/SinglePage.tsx
@@ -1,10 +1,13 @@
 import React, { FC, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { IPost } from "../core/types";
 
 const SinglePage: FC = () => {
     const { id } = useParams();
-    const [ post, setPost ] = useState<IPost|null>(null)
+    const [ post, setPost ] = useState<IPost|null>(null);
+    const navigate = useNavigate();
+
+    const goBack = () => navigate(-1)
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -14,6 +17,7 @@ const SinglePage: FC = () => {
 
     return (
         <div className="single-page" style={{maxWidth: '1000px'}}>
+            <button onClick={goBack}>Go Back</button>
             <h2 style={{textAlign: "center", marginBottom: '30px', fontSize: '48px'}}>{post?.title}</h2>
             <p style={{textAlign: "center", fontSize: '30px', marginBottom: '30px'}}>{post?.body}</p>
             <Link to={`/posts/${id}/edit`} >Edit this post</Link>
